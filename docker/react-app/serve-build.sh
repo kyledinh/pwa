42959d825b89e99a09581f343d26c3bb6191a4e8
Kyle Dinh <kyledinh@gmail.com>
2018-11-02 12:51:57 -0700

    use /usr/src/app

--- docker/react-app/serve-build.sh
+++ docker/react-app/serve-build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-open http://127.0.0.1:9000
-cmd='some-build-serve-cmd --hostname 0.0.0.0 --port 9000 blahblah'
-docker exec -w /child/dirpath/src -it docker_react-app_1 $cmd
+cmd1='npm run build'
+docker exec -w /usr/src/app -it docker_react-app_1 $cmd1
+
+cmd2="webpack-dev-server --mode=production --host 0.0.0.0 --port 3000 --content-base build/"
+docker exec -w /usr/src/app -it docker_react-app_1 $cmd2
+
+open http://127.0.0.1:3000
--- docker/react-app/ssh-container.sh
+++ docker/react-app/ssh-container.sh
@@ -1,3 +1,3 @@
 #1/bin/bash
 
-docker exec -w /opt/app -it docker_react-app_1 bash
+docker exec -w /usr/src/app -it docker_react-app_1 bash
